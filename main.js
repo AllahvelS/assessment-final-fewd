f4b6e24ee7354fcaa2314abe78285b55aa06bdb7
AllahvelS <allahvelsalisbury@pursuit.org>
2023-04-05 12:48:11 -0400

    Review button edited and adjusted to give user an alert when pressing submit without inputting information.

--- main.js
+++ main.js
@@ -1,46 +1,68 @@
-
-
-
-
+const selectMenu = document.querySelector("#titles")
+const options = document.querySelector("option")
+const displayInfo = document.querySelector("#display-info")
+const reviewBtn = document.querySelector("#review-submit")
+const revList = document.querySelector("#review-list")
+const form = document.querySelector("form")
+const revInput = document.getElementById("review")
+const resetBtn = document.querySelector("#reset-reviews")
+const people = document.getElementById("people")
+const peopleBtn = document.getElementById("show-people")
 
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
-  // Select the elements working with
-  //movie titles
-  const selectMenu = document.getElementById('titles');
-  //movie description
-  const displayInfo = document.getElementById('display-info');
+fetch("https://resource-ghibli-api.onrender.com/films/")
+.then((response) => response.json())
+.then((data) => {
+const movies = data
+    selectedMovie(data)
 
-  // Fetch the movie data from the API
-  fetch('https://resource-ghibli-api.onrender.com/films/')
-    .then(response => response.json())
-    .then((data) => {
-      // Loop through the movie data and create an option element for each one
-      data.forEach(movie => {
-        const option = document.createElement('option');
-        option.value = movie.id;
-        option.textContent = movie.title;
-        selectMenu.append(option);
-      });
 
-      // Listen for changes to the select menu and update the movie details accordingly
-      selectMenu.addEventListener('change', () => {
-        // Find the selected movie in the data
-        const selectedMovie = data.find(movie => movie.id === selectMenu.value);
+selectMenu.addEventListener("change", (event) =>{
+    let findMov = movies.find(movie => movie.id === selectMenu.value)
+        displayInfo.innerHTML= ` <h3>${findMov.title}</h3>
+        <p>${findMov.release_date}</p>
+        <p>${findMov.description}</p>`
+})
 
-        // Update the display-info section with the movie details
-        displayInfo.innerHTML = `
-          <h3>${selectedMovie.title}</h3>
-          <p>${selectedMovie.release_date}</p>
-          <p>${selectedMovie.description}</p>
-        `;
-      });
+reviewBtn.addEventListener ("click", (event) => {
+    event.preventDefault()
+    let review = revInput.value
+        for (const movie of movies) {
+            if(movie.id === selectMenu.value){
+                let newReview = document.createElement("li")
+                newReview.innerHTML = `<strong>${movie.title}: </strong>${review}`
+                revList.append(newReview)
+            }
+        }
+        if(selectMenu.value === ""){
+            window.alert("Please select a movie first")
+        }
+    form.reset()
+}) 
+
+resetBtn.addEventListener("click", event => {
+    event.preventDefault()
+    revList.innerHTML = ""
+})
+
+})
+.catch(error => console.log(error))
+
+let selectedMovie = movies => {
+    movies.forEach(movie => {
+        let whateverOption = document.createElement("option")
+        whateverOption.value = movie.id
+        whateverOption.textContent = movie.title
+        selectMenu.append(whateverOption)
     })
-    .catch(error => console.log(error))
+}
 }
 
+// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
+// So that testing can work as expected for now
+// A non-hacky solution is being researched
 
 setTimeout(run, 1000);
-
